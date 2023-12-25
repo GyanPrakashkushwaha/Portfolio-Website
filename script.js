bea403204cf0401107f31d0c7be9cf2d08ccd4ed
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-25 19:12:47 +0530

    Learning gsap and building web.......

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    height:"100px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"#body",
+        markers:true,
+        start: "top -10%",
+        end:"top -11%"
+    }
+})
