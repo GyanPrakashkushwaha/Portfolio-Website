022f865c92f0a85a1ec44f7266683a3ea330038f
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-26 10:44:32 +0530

    add all.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 gsap.to("#nav", {
     backgroundColor: "#000",
-    height: "100px",
     duration: 0.5,
+    height: "70px",
     scrollTrigger: {
       trigger: "#nav",
-      scroller: "#body", // Note: Check if this should be a valid selector or use document.body
+      scroller: "body", // Note: Check if this should be a valid selector or use document.body
       markers: true,
       start: "top -10%",
-      end: "top -11%"
+      end: "top -11%",
+      scrub:0
     }
-  });
+});
+
+
+// gsap.to("#nav", {
+//   backgroundColor: "#000",
+//   duration: 0.5,
+//   height: "110px",
+//   scrollTrigger: {
+//     trigger: "#nav",
+//     scroller: "body",
+//     // markers:true,
+//     start: "top -10%",
+//     end: "top -11%",
+//     scrub: 1,
+//   },
+// });
