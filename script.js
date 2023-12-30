fcda0e492b245d45f0eddf8130cb76e1f8418197
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-30 10:50:41 +0530

    add all

--- script.js
+++ script.js
@@ -15,30 +15,30 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-gsap.to("#nav", {
-    backgroundColor: "#000",
+gsap.to(".navbar", {
+    backgroundColor: "rgb(237, 47, 47)",
     duration: 0.5,
     height: "70px",
     scrollTrigger: {
-      trigger: "#nav",
+      trigger: ".navbar",
       scroller: "body", // Note: Check if this should be a valid selector or use document.body
-      markers: true,
+      // markers: true,
       start: "top -10%",
       end: "top -11%",
       scrub:0
     }
 });
 
-gsap.to("#main",{
-  backgroundColor : "#000",
-  duration:0.5,
-  scrollTrigger:{
-    trigger:"#main",
-    scroller:"body",
-    markers:true,
-    start: "top -25%",
-    end:"top -75%",
-    scrub:1
-  }
+// gsap.to("#main",{
+//   backgroundColor : "#000",
+//   duration:0.5,
+//   scrollTrigger:{
+//     trigger:"#main",
+//     scroller:"body",
+//     markers:true,
+//     start: "top -25%",
+//     end:"top -75%",
+//     scrub:1
+//   }
   
-})
+// })
