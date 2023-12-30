1dfc21dded0f4f301945907bfdd7cf2aad39356c
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-31 00:19:10 +0530

    addd

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const randomRotate = Math.random() * 30 - 10; // Random value between -10 and 10
     const randomTranslateY = Math.random() * 20 - 5; // Random value between -5 and 5
     const randomPerspective = Math.random() * 1000 + 500; // Random value between 500 and 1000
+    const randomColor = getRandomColor();
 
     logo.style.transform = `translateX(${randomTranslateX}px) rotate(${randomRotate}deg) translateY(${randomTranslateY}px) perspective(${randomPerspective}px)`;
+    logo.style.accentColor = randomColor;
   });
 
   logo.addEventListener('mouseleave', function () {
@@ -41,8 +43,32 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+
+function getRandomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgba(" + r + ", " + g + ", " + b + ", 0.8)";
+}
+
+function changeColorRandomly() {
+  blur.style.backgroundColor = getRandomColor();
+}
+
+// Change color randomly every second
+setInterval(changeColorRandomly, 900);
+
+
+
 gsap.to(".navbar", {
-    backgroundColor: "rgb(147, 204, 181,0.90)",
+    backgroundColor: "rgba(49, 40, 94, 0.9)",
     duration: 0.5,
     height: "70px",
     scrollTrigger: {
@@ -58,7 +84,7 @@ gsap.to(".navbar", {
 
 gsap.to(".navbar-about", {
   backgroundColor: "rgb(237, 47, 47)",
-  backgroundColor: "rgb(147, 204, 181,0.50)",
+  backgroundColor: "rgb(147, 204, 181,0.02)",
   duration: 0.5,
   height: "70px",
   scrollTrigger: {
