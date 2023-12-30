00e7b1eb2c1416e5b517be2307496bd41adcecdd
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-30 08:56:48 +0530

    all social media accounts added.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const logo = document.getElementById('logo');
+
+  logo.addEventListener('mouseenter', function () {
+    const randomTranslateX = Math.random() * 40 - 20; // Random value between -10 and 10
+    const randomRotate = Math.random() * 30 - 10; // Random value between -10 and 10
+    const randomTranslateY = Math.random() * 20 - 5; // Random value between -5 and 5
+    const randomPerspective = Math.random() * 1000 + 500; // Random value between 500 and 1000
+
+    logo.style.transform = `translateX(${randomTranslateX}px) rotate(${randomRotate}deg) translateY(${randomTranslateY}px) perspective(${randomPerspective}px)`;
+  });
+
+  logo.addEventListener('mouseleave', function () {
+    logo.style.transform = 'none';
+  });
+});
+
 gsap.to("#nav", {
     backgroundColor: "#000",
     duration: 0.5,
